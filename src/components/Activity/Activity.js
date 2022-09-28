626fb79d20339f9c960c4724b534d5b7a88e294e
sufian <sufian4se@gmail.com>
2022-09-28 23:26:45 +0600

    single activity style added and update

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Activity.css';
 
 const Activity = (props) => {
-    const {img, name, durationTime} = props.activity;
+    const {img, name, durationTime, description} = props.activity;
     return (
         <div className='activity'>
             <img src={img} alt="" />
             <div className="activity-text">
                 <h4>{name}</h4>
-                <p>Duration Time: {durationTime} minutes</p>
+                <p>{description}</p>
+                <p><small>Duration Time: {durationTime} minutes</small></p>
                 <button className='list-btn'>Add to List</button>
             </div>
         </div>
